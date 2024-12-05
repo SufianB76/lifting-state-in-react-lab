977636094f50318c6819669c3591ed28b59dfc1d
Sufian <sufianbhatti76@gmail.com>
2024-12-05 10:42:15 -0500

    Homework GA

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,18 @@
+const BurgerStack = ({ ingredients, removeFromBurger }) => {
+    return (
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li
+            key={index}
+            style={{ backgroundColor: ingredient.color }}
+          >
+            {ingredient.name}
+            <button onClick={() => removeFromBurger(index)}>X</button>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  
+  export default BurgerStack;
+  
